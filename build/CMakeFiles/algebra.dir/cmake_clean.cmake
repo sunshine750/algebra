59d97952c8c3bd2284643eeddc66015723527294
sunshine <sunshine@M604.internal.zjusct.io>
2025-04-29 17:44:29 +0800

    added

---
+++ build/CMakeFiles/algebra.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algebra.dir/src/algebra.c.o"
+  "CMakeFiles/algebra.dir/src/algebra.c.o.d"
+  "CMakeFiles/algebra.dir/src/main.c.o"
+  "CMakeFiles/algebra.dir/src/main.c.o.d"
+  "algebra"
+  "algebra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algebra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
